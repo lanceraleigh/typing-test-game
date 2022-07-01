3027ab4eac8de1b39d4eaabf1e2d2088c09f5195
lanceshunnii <99991999+lanceshunnii@users.noreply.github.com>
2022-07-01 10:18:02 -0600

    Making new component

--- src/components/textPrompt.vue
+++ src/components/textPrompt.vue
@@ -2,4 +2,13 @@
   <div class="textPromptBox">
     <p>{{ populatedText }}</p>
   </div>
+  <form action="" id="keyboardForm">
+    <input type="text" id="keyboardInput" @keypress="keystrokeChecker" />
+  </form>
 </template>
+
+<script>
+export default {
+  name: "textPrompt",
+};
+</script>
